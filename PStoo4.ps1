2948833e5965168ac93efaa91c8e10061527f97d
TihkelK <116258789+TihkelK@users.noreply.github.com>
2023-01-10 09:54:58 +0200

    Add files via upload

---
+++ PStoo4.ps1
@@ -0,0 +1,35 @@
+﻿#Mihkel Kuusemäe
+#09.26.23
+#4. Loo PS skript, mis loob failides olevale kasutajale temanimelise kataloogi koos tema andmetega (customers.xml)
+#küsib kasutajalt xml failinime
+#kui fail pole xml, siis programm annab sellest teada ja küsib uuesti
+#kood leiab kliendi nime ning loob sellenimelise kataloogi
+#kliendinimelisse kataloogi salvestatakse kõik andmed tekstikujul (tĆ¤isaadress ja kontakt)
+
+
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+
+$1input = read-host "Faili nimi (.xml)?"
+
+
+if ($1input -notmatch "$_.xml")
+{
+    DO
+    {
+        write-host "Vale fail"
+        $1input = read-host "Vale failinimi, proovi uuesti"
+    }
+   Until ($1input -match "$_.xml")
+}
+
+$xml = [xml](Get-Content $dir\$1input)
+$rows = $xml.customers.customer
+
+foreach ($row in $rows)
+{
+    $muutuja = $row.full_name
+    New-Item $dir\customers\$muutuja -ItemType Directory
+    new-item $dir\customers\$muutuja\$muutuja.txt
+    $row >> $dir\customers\$muutuja\$muutuja.txt
+}
